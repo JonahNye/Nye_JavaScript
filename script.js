c27b5aad1a7be8a772c547061c4893583d5fc327
Jonah Nye <Jonahnye94@gmail>
2018-08-30 10:45:42 -0400

    Completed Javascript, albeit with some alert problems

--- script.js
+++ script.js
@@ -1,23 +1,53 @@
-/* Activity 1 */
 
+
+
+
+/* Activity 1 */
+var givennumber = "Enter a number between 1 and 15, please.";
+var answer = prompt(givennumber);
+
+var sumcollector = 0;
+
+if (answer < 16) {
+for (var i=0; i<=answer; i++) {
+    sumcollector +=i;
+}}
+
+alert(sumcollector);
 
 
 
 
 
 /*Activity 2  */
-var start = prompt("Ready to play?", "yes or no").toLowerCase();
+var start = prompt("Ready to play?", "yes or no");
 var given = "";
 if (start === "no") {
-  Alert("Darn");}
+  Alert(Darn);}
 else if (start == "yes") {
   do {
-    var pleasetype = prompt("Please type a word").toLowerCase();
+    var pleasetype = prompt("Please type a word");
     given = given += pleasetype + " ";
-  } while ((repeat = prompt("How about another round, eh?").toLowerCase() === "yes")
+  } while ((repeat = prompt("How about another round, eh?") === "yes")
 )
   console.log(given);
 }
+
+
+
+/*Activity 3 */
+var givenname = prompt("What's your name?");
+var printname = prompt("Would you like to print your name?");
+
+while (printname.toLowerCase() == "yes") {
+     console.log("Hello. My name is " + givenname);
+     printname = prompt("Would you like to print your name again?");
+     if (printname == "yes") {
+         givenname = givenname + "!";
+     }
+}
+
+
 
 
 
